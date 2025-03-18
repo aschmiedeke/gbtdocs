de059e8ab3b9ffc7f17fb12c14861640fa59f625
Anika Schmiedeke <ajschmiedeke@nrao.edu>
2025-03-18 09:26:51 -0400

    Adding gbtidl to path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../sparrow'))
-
+sys.path.insert(0, os.path.abspath('../../gbtidl'))
 # -- Project information -----------------------------------------------------
 
 project = 'GBT Docs'
